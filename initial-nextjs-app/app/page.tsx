a4bbb0871e6de537796fa3051b899f46cfc17efb
paul-blackwell <paul@paulblackwell.io>
2023-11-20 08:15:34 +0000

    Completed the following lesson for Section 12 Appendix A: + The search var and restaurant card

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -1,36 +1,5 @@
 import { Inter } from '@next/font/google';
 
 export default function Home() {
-  return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* START NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="#" className="font-bold text-gray-700 text-2xl">
-            OpenTable
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border py-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border py-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
-        {/* END NAVBAR */}
-        <main>
-          {/* START HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-            </div>
-          </div>
-          {/* END HEADER */}
-        </main>
-      </main>
-    </main>
-  );
+  return (<main></main>);
 }
