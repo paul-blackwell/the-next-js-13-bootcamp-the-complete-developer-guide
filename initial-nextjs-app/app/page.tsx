1389451273e8449e7019716774abccc584d7ef6f
paul-blackwell <paul@paulblackwell.io>
2023-11-22 08:24:03 +0000

    Completed the following lesson for section 12 Appendix A

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -20,10 +20,55 @@ export default function Home() {
         </nav>
         {/* END NAVBAR */}
         {/* START HEADER */}
-        <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full justify-center items-center">
-          <h1 className="text-7xl text-white capitalize text-shadow text-center">Meadow Kitchen (Toronto)</h1>
+        <div className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+            <h1 className="text-7xl text-white capitalize text-shadow text-center">
+              Meadow Kitchen (Toronto)
+            </h1>
+          </div>
         </div>
         {/* END HEADER */}
+        {/* START DESCRIPTION PORTION */}
+        <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
+          <div className="bg-white w-[70%] rounded p-3 shadow">
+            {/* START RESTAURANT NAVBAR */}
+            <nav className="flex text-reg border-b pb-2">
+              <a href="#" className="mr-7">
+                Overview
+              </a>
+              <a href="#" className="mr-7">
+                Menu
+              </a>
+            </nav>
+            {/* END RESTAURANT NAVBAR */}
+            {/* START TITLE */}
+            <div className="mt-4 border-b pb-6">
+              <h2 className="font-bold text-6xl">Milestone Grill</h2>
+            </div>
+            {/* END TITLE */}
+            {/* START RATINGS */}
+            <div className="flex items-end">
+              <div className="ratings mt-2 flex items-center">
+                <p>*****</p>
+                <p className="text-reg ml-3">4.9</p>
+              </div>
+              <div>
+                <p className="text-reg ml-3">600 reviews</p>
+              </div>
+            </div>
+            {/* END RATINGS */}
+            {/* START DESCRIPTION */}
+            <div className="mt-4">
+              <p className="text-lg font-light">
+                The Dining Rooms located in Albion House, right beside the sea in Ramsgate. The
+                restaurant is composed of elegant Georgian rooms overlooking Ramsgate's Royal
+                Harbour and the impressive Mayors' Chamber which has a great, relaxed atmosphere.
+              </p>
+            </div>
+            {/* END DESCRIPTION */}
+          </div>
+        </div>
+        {/* END DESCRIPTION PORTION */}
       </main>
     </main>
   );
