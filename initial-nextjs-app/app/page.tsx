c3e7bee492999ec47869bdc7e37f610f27194ed5
paul-blackwell <paul@paulblackwell.io>
2023-11-23 08:17:33 +0000

    Completed the following lesson for section 12 Appendix Appendix + The image gallery

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -66,6 +66,68 @@ export default function Home() {
               </p>
             </div>
             {/* END DESCRIPTION */}
+            {/* START IMAGES */}
+            <h3 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">7 photos</h3>
+            <div className="flex flex-wrap">
+              <img
+                className="w-56 h-44 mr-1 object-cover"
+                src="https://resizer.otstatic.com/v2/photos/xlarge/3/48135639.webp"
+                alt=""
+              />
+              <img
+                className="w-56 h-44 mr-1 object-cover"
+                src="https://resizer.otstatic.com/v2/photos/xlarge/2/42309022.webp"
+                alt=""
+              />
+              <img
+                className="w-56 h-44 mr-1 object-cover"
+                src="https://resizer.otstatic.com/v2/photos/xlarge/2/48147648.webp"
+                alt=""
+              />
+              <img
+                className="w-56 h-44 mr-1 object-cover"
+                src="https://resizer.otstatic.com/v2/photos/xlarge/3/48147646.webp"
+                alt=""
+              />
+              <img
+                className="w-56 h-44 mr-1 object-cover"
+                src="https://resizer.otstatic.com/v2/photos/xlarge/2/48135424.webp"
+                alt=""
+              />
+            </div>
+            {/* END IMAGES */}
+            {/* START REVIEWS */}
+            <div>
+              <h5 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                What 100 people are saying
+              </h5>
+              {/* START REVIEW CARD */}
+              <div className="border-b pb-7 mb-7">
+                <div className="flex">
+                  <div className="w-1/6 flex flex-col items-center">
+                    <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                      <span className="text-white text-2xl">JS</span>
+                    </div>
+                    <p className="text-center">James Smith</p>
+                  </div>
+                  <div className="ml-10 w-5/6">
+                    <div className="flex items-center">
+                      <div className="flex mr-5">*****</div>
+                    </div>
+                    <div className="mt-5">
+                      <p className="text-lg font-light">
+                        Great service, very friendly, had looked at our dietary requirements
+                        beforehand. The halloumi burger was tasty. The only downside is that there
+                        could have been more vegetarian and vegan options. Breakfast was overpriced
+                        for what it was and not many vegan options.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* END REVIEW CARD */}
+            </div>
+            {/* END REVIEWS */}
           </div>
         </div>
         {/* END DESCRIPTION PORTION */}
