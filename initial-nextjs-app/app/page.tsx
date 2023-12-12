0b30585d225451e42b8522af49005f30949ea16c
paul-blackwell <paul@paulblackwell.io>
2023-12-12 08:10:35 +0000

    Completed the following lessons for section 2:
    + Challenge Solution
    + The Two Ways of Navigating Between Routes
    + The Link Component

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -1,4 +1,4 @@
-import { Inter } from '@next/font/google';
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,9 +6,9 @@ export default function Home() {
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* START NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
-          <a href="#" className="font-bold text-gray-700 text-2xl">
+          <Link href="/" className="font-bold text-gray-700 text-2xl">
             OpenTable
-          </a>
+          </Link>
           <div>
             <div className="flex">
               <button className="bg-blue-400 text-white border py-1 px-4 rounded mr-3">
@@ -43,24 +43,26 @@ export default function Home() {
           <div className="py-3 px-36 mt-10 flex flex-wrap">
             {/* START OF CARD */}
             <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-              <img
-                src="https://resizer.otstatic.com/v2/photos/wide-huge/2/52592579.webp"
-                alt=""
-                className="w-full h-36"
-              />
-              <div className="p-1">
-                <h3 className="font-bold text-2xl mb-2">Meadow Kitchen</h3>
-                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
-                  <p className="ml-2">77 reviews</p>
+              <Link href="/restaurant/milestones-grill">
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/52592579.webp"
+                  alt=""
+                  className="w-full h-36"
+                />
+                <div className="p-1">
+                  <h3 className="font-bold text-2xl mb-2">Meadow Kitchen</h3>
+                  <div className="flex items-start">
+                    <div className="flex mb-2">*****</div>
+                    <p className="ml-2">77 reviews</p>
+                  </div>
+                  <div className="flex text-reg font-light capitalize">
+                    <p className="mr-3">Mexican</p>
+                    <p className="mr-3">$$$$</p>
+                    <p>Toronto</p>
+                  </div>
+                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
                 </div>
-                <div className="flex text-reg font-light capitalize">
-                  <p className="mr-3">Mexican</p>
-                  <p className="mr-3">$$$$</p>
-                  <p>Toronto</p>
-                </div>
-                <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-              </div>
+              </Link>
             </div>
             {/* END OF CARD */}
           </div>
