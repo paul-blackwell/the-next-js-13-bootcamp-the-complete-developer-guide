ce7d965cb36338f4291ed5d43172ed39ed3f7416
paul-blackwell <paul@paulblackwell.io>
2023-12-14 08:26:54 +0000

    Completed the following lesson for section 3:
    + Refactoring Our Pages (50%)

--- initial-nextjs-app/app/restaurant/[slug]/page.tsx
+++ initial-nextjs-app/app/restaurant/[slug]/page.tsx
@@ -1,33 +1,18 @@
-import Link from "next/link";
-import NavBar from "../../components/NavBar";
+import Link from 'next/link';
+import NavBar from '../../components/NavBar';
+import Header from './components/Header';
+import RestaurantNavBar from './components/RestaurantNavBar';
 
 export default function RestaurantDetails() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
-        {/* START HEADER */}
-        <div className="h-96 overflow-hidden">
-          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-            <h1 className="text-7xl text-white capitalize text-shadow text-center">
-              Meadow Kitchen (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* END HEADER */}
+        <Header />
         {/* START DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
           <div className="bg-white w-[70%] rounded p-3 shadow">
-            {/* START RESTAURANT NAVBAR */}
-            <nav className="flex text-reg border-b pb-2">
-              <Link href="/restaurant/milestone-grill" className="mr-7">
-                Overview
-              </Link>
-              <Link href="/restaurant/milestone-grill/menu" className="mr-7">
-                Menu
-              </Link>
-            </nav>
-            {/* END RESTAURANT NAVBAR */}
+            <RestaurantNavBar />
             {/* START TITLE */}
             <div className="mt-4 border-b pb-6">
               <h2 className="font-bold text-6xl">Milestone Grill</h2>
