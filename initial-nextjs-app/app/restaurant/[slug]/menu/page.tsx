195e95c1535bee711758b38cea62ba56d7e810b0
paul-blackwell <paul@paulblackwell.io>
2023-12-21 07:53:29 +0000

    COmpleted the following lesson for section 4: + Nested Components

--- initial-nextjs-app/app/restaurant/[slug]/menu/page.tsx
+++ initial-nextjs-app/app/restaurant/[slug]/menu/page.tsx
@@ -1,17 +1,11 @@
-import Header from '../components/Header';
 import Menu from '../components/Menu';
 import RestaurantNavBar from '../components/RestaurantNavBar';
 
 export default function RestaurantMenu() {
   return (
-    <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Menu />
-        </div>
-      </div>
-    </>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
   );
 }
--- initial-nextjs-app/app/restaurant/[slug]/page.tsx
+++ initial-nextjs-app/app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCard from './components/ReservationCard';
@@ -10,20 +9,15 @@ import Title from './components/Title';
 export default function RestaurantDetails() {
   return (
     <>
-      <Header />
-      {/* START DESCRIPTION PORTION */}
-      <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <ReservationCard />
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
       </div>
-      {/* END DESCRIPTION PORTION */}
+      <ReservationCard />
     </>
   );
 }
