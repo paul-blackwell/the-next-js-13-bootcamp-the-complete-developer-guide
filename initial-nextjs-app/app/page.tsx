e78f4c3d99db438b943e46feb250dd7c1765e9c8
paul-blackwell <paul@paulblackwell.io>
2024-01-01 10:03:26 +0000

    Completed the following lesson for section 5:
    + A Little TypeScript Lesson

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -1,28 +1,46 @@
-
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
+
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine,
+  location: Location;
+  price: PRICE;
+} 
 
 const prisma = new PrismaClient();
 
-const fetchRestaurants = async () => {
-  const restaurants = await prisma.restaurant.findMany();
+const fetchRestaurants = async (): Promise<RestaurantCardType []> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    }
+  });
 
   return restaurants;
-}
+};
 
 export default async function Home() {
-
   const restaurants = await fetchRestaurants();
 
-  console.log(restaurants)
+  console.log(restaurants);
 
   return (
     <main>
       <Header />
       {/* START OF CARDS */}
       <div className="py-3 px-36 mt-10 flex flex-wrap">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
       {/* END OF CARDS */}
     </main>
