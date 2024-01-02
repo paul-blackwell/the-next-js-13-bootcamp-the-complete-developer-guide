c5f7c0c0766fdc67f46f842edd622fc04b9cdc6f
paul-blackwell <paul@paulblackwell.io>
2024-01-02 08:04:01 +0000

    Completed the following lesson for section 5:
    + Rendering the Fetched Data

--- initial-nextjs-app/app/page.tsx
+++ initial-nextjs-app/app/page.tsx
@@ -9,6 +9,7 @@ export interface RestaurantCardType {
   cuisine: Cuisine,
   location: Location;
   price: PRICE;
+  slug: string;
 } 
 
 const prisma = new PrismaClient();
@@ -22,6 +23,7 @@ const fetchRestaurants = async (): Promise<RestaurantCardType []> => {
       cuisine: true,
       location: true,
       price: true,
+      slug: true,
     }
   });
 
