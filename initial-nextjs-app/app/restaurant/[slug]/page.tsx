7e4e9f499cfb7fc0afcefe9b961fb9b3d80b0627
paul-blackwell <paul@paulblackwell.io>
2024-01-02 08:25:20 +0000

    Completed the following lessons for section 5:
    + Fetching a Restaurant By Slug
    + Populating the Data in the Restaurant Page

--- initial-nextjs-app/app/restaurant/[slug]/page.tsx
+++ initial-nextjs-app/app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import Description from './components/Description';
 import Images from './components/Images';
 import Rating from './components/Rating';
@@ -6,7 +7,42 @@ import RestaurantNavBar from './components/RestaurantNavBar';
 import Reviews from './components/Reviews';
 import Title from './components/Title';
 
-export default function RestaurantDetails() {
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  // TODO
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
+
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log(restaurant);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
