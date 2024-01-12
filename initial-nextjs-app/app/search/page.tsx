e3218f6896114fefc17e86b615ac61c99c40ee0d
paul-blackwell <paul@paulblackwell.io>
2024-01-12 08:31:36 +0000

    Completed the following lesson for section 6:
    + Solution for Challenge 2

--- initial-nextjs-app/app/search/page.tsx
+++ initial-nextjs-app/app/search/page.tsx
@@ -1,21 +1,50 @@
+import { PrismaClient } from '@prisma/client';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 import SearchSidebar from './components/SearchSidebar';
 
-export default function Search({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = async (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!city) await prisma.restaurant.findMany({ select });
+
+  return await prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city?.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+export default async function Search({ searchParams }: { searchParams: { city: string } }) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+  console.log(restaurants);
+
   return (
     <>
-      <Header searchParams={searchParams} />
+      <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSidebar />
         <div className="w-5/6">
-          {/* START RESTAURANT CARD */}
-          <RestaurantCard />
-          {/* END RESTAURANT CARD */}
+          {restaurants.length ? (
+            <RestaurantCard />
+          ) : (
+            <p>Sorry, we found no restaurants in that area.</p>
+          )}
         </div>
       </div>
     </>
