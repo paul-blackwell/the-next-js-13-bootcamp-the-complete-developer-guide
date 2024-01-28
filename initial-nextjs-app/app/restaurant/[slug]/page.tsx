32fa2e93844721418d441b37430fff99b7773a7d
paul-blackwell <paul@paulblackwell.io>
2024-01-28 10:11:20 +0000

    Completed the following lessons for section 7:
    + Handling Error States
    + Handling Not Found States

--- initial-nextjs-app/app/restaurant/[slug]/page.tsx
+++ initial-nextjs-app/app/restaurant/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { PrismaClient, Review } from '@prisma/client';
+import { notFound } from 'next/navigation';
 import Description from './components/Description';
 import Images from './components/Images';
 import Rating from './components/Rating';
@@ -33,9 +34,8 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
     },
   });
 
-  // TODO
   if (!restaurant) {
-    throw new Error();
+    notFound();
   }
 
   return restaurant;
