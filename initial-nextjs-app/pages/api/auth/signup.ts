6933f0b90b5a63f2ce21aaa63522a6502e85f96a
paul-blackwell <paul@paulblackwell.io>
2024-02-06 08:29:18 +0000

    Completed the following lesson in section 8:
    + Validating the User's Input (80% completed)

--- initial-nextjs-app/pages/api/auth/signup.ts
+++ initial-nextjs-app/pages/api/auth/signup.ts
@@ -1,9 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import validator from 'validator';
+import isStrongPassword from 'validator/lib/isStrongPassword';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if(req.method === 'POST') {
+  const { firstName, lastName, phone, email, city, password } = req.body;
+
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: 'First name is invalid',
+    },
+    {
+      valid: validator.isLength(lastName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: 'Last name is invalid',
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: 'Email is invalid',
+    },
+    {
+      valid: validator.isMobilePhone(phone, 'any'),
+      errorMessage: 'Phone number is invalid',
+    },
+    {
+      valid: validator.isLength(city, {
+        min: 1,
+      }),
+      errorMessage: 'City is invalid',
+    },
+    {
+      valid: isStrongPassword(password),
+      errorMessage: 'Password invalid',
+    },
+  ];
+
+  if (req.method === 'POST') {
     res.status(200).json({
-      hello: 'there',
+      hello: 'test',
     });
   }
 };
